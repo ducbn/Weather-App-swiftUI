4ec2692b865f4d07cea5dc4cfee88b903eb87af3
Bùi Ngọc Đức <ducbui27924@gmail.com>
2024-07-19 15:50:58 +0700

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by Bui Ngoc Duc on 19/7/24.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
