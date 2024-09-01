2e8c550a346d787a1708c82b27693ce93e343ecb
BuiNgocDuc <ducbui27924@gmail.com>
2024-09-01 14:39:19 +0700

    final version

--- weather/View/ContentView.swift
+++ weather/View/ContentView.swift
@@ -1,117 +1,114 @@
 import SwiftUI
 
 struct ContentView: View {
-    @ObservedObject var viewModel = ForecastViewModel()
+    
+    @EnvironmentObject var listView: ListViewModel
+    @ObservedObject var weather = ForecastViewModel()
     
     var body: some View {
         NavigationView {
-            ZStack{
-//                Image("cloudyday")
-//                    .resizable()
-//                    .scaledToFill()
-//                    .edgesIgnoringSafeArea(.all)
-                
-                VStack {
+            TabView{
+                ForEach(listView.items) { item in
                     VStack {
-                        Text("Ha Noi")
-                            .font(.largeTitle)
-                            .padding(.top)
-                            .foregroundColor(.black)
-                        
-                        Text("\(Int(viewModel.forecast?.current.temp ?? 0))°C")
-                            .font(.largeTitle)
-                            .foregroundColor(.black)
-                        
-                        if let currentWeatherDescription = viewModel.forecast?.current.weather.first?.description {
-                            Text("\(currentWeatherDescription)")
+                        VStack {
+                            Text("\(item.nameCity)")
+                                .font(.largeTitle)
+                                .padding(.top)
                                 .foregroundColor(.black)
+                            
+                            Text("\(Int(weather.forecast?.current.temp ?? 0))°C")
+                                .font(.largeTitle)
+                                .foregroundColor(.black)
+                            
+                            if let currentWeatherDescription = weather.forecast?.current.weather.first?.description {
+                                Text("\(currentWeatherDescription)")
+                                    .foregroundColor(.black)
+                            }
+                            
                         }
-                        
-                    }
 
-                    ScrollView {
-                        if let forecast = viewModel.forecast {
-                            ScrollView(.horizontal, showsIndicators: false) {
-                                HStack(spacing: 5) {
-                                    ForEach(forecast.hourly.prefix(24), id: \.dt) { hour in
-                                        VStack(alignment: .center) {
-                                            Text("\(formatDate(from: hour.dt, with: "HH"))")
+                        ScrollView {
+                            if let forecast = weather.forecast {
+                                ScrollView(.horizontal, showsIndicators: false) {
+                                    HStack(spacing: 5) {
+                                        ForEach(forecast.hourly.prefix(24), id: \.dt) { hour in
+                                            VStack(alignment: .center) {
+                                                Text("\(formatDate(from: hour.dt, with: "HH"))")
+                                                    .foregroundColor(.white)
+                                                
+                                                if let icon = hour.weather.first?.icon {
+                                                    AsyncImage(url: URL(string: "https://openweathermap.org/img/wn/\(icon)@2x.png")) {image in image
+                                                            .image?.resizable() //cho phép hình ảnh thay đổi kích thước.
+                                                            .scaledToFit() // giữ nguyên tỷ lệ khung hình của hình ảnh và đảm bảo hình ảnh vừa với kích thước khung bạn đã đặt (.frame(width: 50, height: 50)).
+                                                            .frame(width: 50, height: 50)
+                                                    }
+                                                }
+                                                
+                                                Text("\(Int(hour.temp))°C")
+                                                    .font(.subheadline)
+                                                    .foregroundColor(.white)
+                                            }
+                                            .padding()
+                                        }
+                                    }
+                                    
+                                }
+                                .background(Color.gray.opacity(0.7))
+                                .cornerRadius(20)
+                                .padding()
+                                
+                                VStack {
+                                    ForEach(forecast.daily.prefix(10), id: \.dt) { day in
+                                        HStack {
+                                            Text("\(formatDate(from: day.dt, with: "EEE"))")
                                                 .foregroundColor(.white)
-                                            
-                                            if let icon = hour.weather.first?.icon {
-                                                AsyncImage(url: URL(string: "https://openweathermap.org/img/wn/\(icon)@2x.png")) {image in image
-                                                        .image?.resizable() //cho phép hình ảnh thay đổi kích thước.
-                                                        .scaledToFit() // giữ nguyên tỷ lệ khung hình của hình ảnh và đảm bảo hình ảnh vừa với kích thước khung bạn đã đặt (.frame(width: 50, height: 50)).
+                                            Spacer()
+                                            if let icon = day.weather.first?.icon {
+                                                AsyncImage(url: URL(string: "https://openweathermap.org/img/wn/\(icon)@2x.png")){img in img
+                                                        .image?.resizable()
+                                                        .scaledToFill()
                                                         .frame(width: 50, height: 50)
                                                 }
                                             }
-                                            
-                                            Text("\(Int(hour.temp))°C")
-                                                .font(.subheadline)
+                                            Text("\(Int(day.temp.day)) °C")
                                                 .foregroundColor(.white)
+                                            
                                         }
                                         .padding()
+                                        .cornerRadius(10)
                                     }
                                 }
-                                
-                            }
-                            .background(Color.gray.opacity(0.7))
-                            .cornerRadius(20)
-                            .padding()
-                            
-                            VStack {
-                                ForEach(forecast.daily.prefix(10), id: \.dt) { day in
-                                    HStack {
-                                        Text("\(formatDate(from: day.dt, with: "EEE"))")
-                                            .foregroundColor(.white)
-                                        Spacer()
-                                        if let icon = day.weather.first?.icon {
-                                            AsyncImage(url: URL(string: "https://openweathermap.org/img/wn/\(icon)@2x.png")){img in img
-                                                    .image?.resizable()
-                                                    .scaledToFill()
-                                                    .frame(width: 50, height: 50)
-                                            }
-                                        }
-                                        Text("\(Int(day.temp.day)) °C")
-                                            .foregroundColor(.white)
-                                        
-                                    }
-                                    .padding()
-                                    .cornerRadius(10)
-                                }
-                            }
-                            .background(Color.gray.opacity(0.7))
-                            .cornerRadius(10)
-                            .padding(.horizontal)
+                                .background(Color.gray.opacity(0.7))
+                                .cornerRadius(10)
+                                .padding(.horizontal)
 
-                        } else {
-                            Text("Loading...!!!")
-                                .font(.largeTitle)
-                                .padding()
+                            } else {
+                                Text("Loading...!!!")
+                                    .font(.largeTitle)
+                                    .padding()
+                            }
                         }
+                    }
+                    .onAppear {
+                        weather.fetchForecast(latitude: item.lat, longitude: item.lon)
+                    }
+                            
+                    
+                }
+            }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationLink(destination: SearchLocation()) {
+                        Image(systemName: "plus")
                     }
                 }
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        NavigationLink(destination: SearchLocation()) {
-                            Image(systemName: "plus")
-                        }
-                    }
-                    ToolbarItem(placement: .navigationBarLeading) {
-                        NavigationLink(destination: SearchLocation()) {
-                                Image(systemName: "list.dash")
-                        }
-                    }
-               }
-                .onAppear {
-                    viewModel.fetchForecast(latitude: 20.961123, longitude: 105.734478)
-                }
+        
             }
         }
     }
 }
 
-// Convert timestamp to date string with given format
 func formatDate(from timestamp: Int, with format: String) -> String {
     let date = Date(timeIntervalSince1970: TimeInterval(timestamp))
     let dateFormatter = DateFormatter()
@@ -121,5 +118,5 @@ func formatDate(from timestamp: Int, with format: String) -> String {
 
 #Preview {
     ContentView()
+        .environmentObject(ListViewModel())
 }
-
--- weather/weatherApp.swift
+++ weather/weatherApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct weatherApp: App {
     var body: some Scene {
         WindowGroup {
-            SearchLocation()
+            ContentView()
+                .environmentObject(ListViewModel())
+            
         }
     }
 }
